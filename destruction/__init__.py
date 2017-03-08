6ba20fd7465c777c18468727d773681891645a3b
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-03-08 20:59:36 +0100

    Add alternative range
    The best thing about this is: if you import destruction into the ipython
    shell it crashes immediately.

--- destruction/__init__.py
+++ destruction/__init__.py
@@ -9,5 +9,10 @@ else:
 from destruction.AlternativeFloat import AlternativeFloat
 builtins.float = AlternativeFloat
 
+
 from destruction.AlternativeMath import AlternativeMath
 sys.modules['math'] = AlternativeMath()
+
+
+from destruction.AlternativeRange import alternative_range
+builtins.range = alternative_range
