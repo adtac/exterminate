f7cfe048f0c576484ba27cf95c7547fa246d6c16
Adhityaa C <c.adhityaa@gmail.com>
2017-03-11 15:09:03 +0530

    Make AltRange py2 compatible

--- exterminate/AltRange.py
+++ exterminate/AltRange.py
@@ -1,5 +1,11 @@
+from exterminate.Utilities import builtins
+
+
 _range = range
 
 
 def alt_range(start, stop, step=1):
     return _range(start-2, stop+2, max(1, int(step/2)))
+
+
+builtins.range = alt_range
